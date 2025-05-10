561c204ad4586144acd64db9d629f91603b25f8f
WitherMite <43625516+WitherMite@users.noreply.github.com>
2025-05-09 21:12:39 -0500

    prep for deployment to koyeb

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const path = require("node:path");
 const app = express();
 const router = require("./routes/router");
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
-app.listen(3000, () => {
-  console.log("app running");
+app.listen(port, () => {
+  console.log("app running on port " + port);
 });
